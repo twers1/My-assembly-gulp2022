b58c79f9e1b9c7d6279758bfe3a4e9e24c4a39c6
Sima <tarverdans15@gmail.com>
2022-12-02 19:48:05 +0300

    update plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
+"use strict"
+
 const {src, dest} = require('gulp')
-const sass = require('gulp-sass')
-const csso = require('gulp-csso')
-const include = require('gulp-file-include') // пакет, отвечающий за соединения данных файлов
-const htmlmin = require('gulp-htmlmin')
+const gulp = require('gulp')
+const sass = require('gulp-sass')(requare('sass'))
 const del = require('del')
-const sync = require('browser-sync').create()
+const autoprefixer = require('gulp-autoprefixer')
+const cssbeautify = require('gulp-cssbeautify')
+const removeComments = require('gulp-strip-css-comments')
+const rename = require('gulp-rename')
+const cssnano = require('gulp-cssnano')
+const uglify = require('gulp-uglify')
+const plumber = require('gulp-prumber')
+const panini = require('panini')
+const imagemin = require('gulp-imagemin')
+const browserSync = require('browser-sync').create()
 
-function html() {
-    return src('src/**.html')  // читай все файлв с расширением html 
-        .pipe(include({
-            prefix: '@@'
-        }))
-        .pipe(dest('dist'))
+const path = {
+    build: {
+        html:'dist/', // build - то, что будет получаться у нас на выходе (там, где все сжато)
+        css: 'dist/assets/css/',
+        js: 'dist/assets/js/',
+        
+    }
 }
-
-exports.html = html
